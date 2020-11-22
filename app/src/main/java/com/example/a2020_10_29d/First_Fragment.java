344dfecf5ded2a48ee8a669ddfe93dc8cad5a121
steven8602 <steven8602@naver.com>
2020-11-23 00:17:53 +0900

    Pop

--- app/src/main/java/com/example/a2020_10_29d/First_Fragment.java
+++ app/src/main/java/com/example/a2020_10_29d/First_Fragment.java
@@ -15,50 +15,30 @@ import android.webkit.WebViewClient;
 
 
 public class First_Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    WebView mWebView;
 
     public First_Fragment() {
         // Required empty public constructor
     }
 
-
-    // TODO: Rename and change types and number of parameters
-    public static First_Fragment newInstance(String param1, String param2) {
-        First_Fragment fragment = new First_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    WebView mWebView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first_, container, false);
-        mWebView = view.findViewById(R.id.webview);
+        mWebView = (WebView) view.findViewById(R.id.webview);
         mWebView.setWebViewClient(new WebViewClient());
+      //  mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         mWebView.getSettings().setJavaScriptEnabled(true);
+
+
         mWebView.loadUrl("https://www.naver.com/");
         return view;
     }
